8edad6b9fee650b6b5d91a8a2e4cf3b9dfb747d5
JunaidShafi <er.junaidshafi@gmail.com>
2024-09-05 20:17:43 +0530

    This is my react learning

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hello from "./Components/Hello";
+function App() {
+  return (
+    <div>
+      <Hello name="Junaid" message="Hi There! " />
+      <Hello name="Tawqeer" message="How are you?" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello(props) {
+  console.log(props);
+  return (
+    <h1>
+      {props.message} {props.name}
+    </h1>
+  );
+}
+export default Hello;
